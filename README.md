bfda5e2c3e829d3dcd074f0689cc64eedc756a3e
Cimbali <me@cimba.li>
2023-12-13 13:08:07 +0000

    Simplify to the max

--- README.md
+++ README.md
@@ -5,7 +5,3 @@ In case the Advanced Outlook.com security for Microsoft 365 subscribers is turne
 
 This add-on undos that process so that the original link is instead used in the message.
 It decodes the rewritten URL and inserts the original URL instead.
-
-When a url was decoded, an unlock icon is shown next to the link.
-
-
--- src/background.js
+++ src/background.js
@@ -3,18 +3,46 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-/**
- * Execute the startup handler whenever Thunderbird starts.
- */
-document.addEventListener("DOMContentLoaded", () => {
+
+function cleanup(doc) {
+  const wrapChars = ['', '""', "''", '<>'];
+
+  // Match links to safelinks.protection.outlook.com and change them.
+  for (let link of doc.querySelectorAll('a[href*="safelinks.protection.outlook.com"]')) {
+    const trackURL = link.href;
+    try {
+      const cleanURL = new URL(trackURL).searchParams.get("url");
+      link.href = new URL(cleanURL).toString();
+    } catch (error) {
+      // Could be just mentioning the safelinks service and not really a tracked link
+      console.warn(`Failed getting clean URL for "${trackURL}": ${error.message}`)
+      continue;
+    }
+
+    // Identify which pattern wraps the link text
+    const wrap = 1 + wrapChars.slice(1).indexOf([
+      link.textContent.slice(0, 1),
+      link.textContent.slice(-1),
+    ].join(''));
+
+    // Replace link text if it matches the tracked link
+    if (trackURL == link.textContent.slice(!!wrap, link.textContent.length - !!wrap)) {
+      link.textContent = [
+        wrapChars[wrap].slice(0, 1),
+        link.href,
+        wrapChars[wrap].slice(1),
+      ].join('');
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
   messenger.messageDisplayScripts.register({
-    js: [{ file: "/src/safelinks-offr-utils.js" }, { file: "/src/safelinks-offr-display.js" }],
-    css: [{ file: "/src/safelinks-offr.css" }],
+    js: [{ code: `(${cleanup.toString()})(document);` }],
   });
 
   messenger.composeScripts.register({
-    js: [{ file: "/src/safelinks-offr-utils.js" }, { file: "/src/safelinks-offr-compose.js" }],
-    css: [{ file: "/src/safelinks-offr.css" }],
+    js: [{ code: `(${cleanup.toString()})(document);` }],
   });
 });
 
